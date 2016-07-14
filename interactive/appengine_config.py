b617634925a3662ed0d535474961aae16fbdf994
Jay Teguh <teguhwpurwanto@gmail.com>
2016-07-14 15:15:33 +0700

    chore: Setup for gcloud done

---
+++ interactive/appengine_config.py
@@ -0,0 +1,5 @@
+from google.appengine.ext import vendor
+# insert `lib` as a site directory so our `main` module can load
+# third-party libraries, and override built-ins with newer
+# versions.
+vendor.add('lib')
